0f41a7b37281d13611aba6c99e63e037c06789bf
Godson Rapture Chijioke <coolpythoncodes@gmail.com>
2020-08-18 19:13:19 +0100

    clear input field on submission

--- main.js
+++ main.js
@@ -8,16 +8,15 @@ let loss = 0;
 
 const success = ()=>{
     win+=1;
-    let h2 = document.querySelector('h2');
-    h2.innerText = `Hurray!!! Correct guess. Number of wins ${win}`;   
+    let h3 = document.querySelector('h3');
+    h3.innerText = `Hurray!!! ${userInput.value} was the Correct guess. Number of wins ${win}`;   
     
 }
 
 const failed = ()=>{
     loss+=1; 
-    let h2 = document.querySelector('h2');
-    h2.innerText = `Sorry, that was not the correct guess. Number of loss ${loss}`;
-      
+    let h3 = document.querySelector('h3');
+    h3.innerText = `Sorry, ${userInput.value} was not the correct guess. Number of loss ${loss}`;      
 }
 
 form.addEventListener('submit',(e)=>{
@@ -29,9 +28,11 @@ form.addEventListener('submit',(e)=>{
 
     if (Number(userInput.value) === randomNumber()) {
         success();
+        userInput.value = '';
    
     }else{
         failed();
+        userInput.value = '';
     }
     
 })
